8f73d587c72adb2de744b62957208dcc6fb61706
Yash Tripathi <yashtripathi2205@gmail.com>
2023-03-20 15:18:34 +0530

    initial commit

---
+++ Basic_Codes.py
@@ -0,0 +1,20 @@
+# Below is a code to print two numbers while taking input from the user
+
+a = input("Enter first number: ") #input always taken as string
+b = input("Enter second number: ")
+a = float(a) #converting string to float
+b = float(b) 
+print('The Sum of the two numbers is ',a+b) 
+
+# Below is a code to find remainder
+
+a = input("Enter first number: ") #input always taken as string
+b = input("Enter second number: ")
+a = float(a) #converting string to float
+b = float(b) 
+print("The reaminder is ",a%b)
+
+# Below is a code to check data type of a number
+
+a = 45
+print(type(a))
