5c40f5f735adddf0e122b082e378dabe6ed7927e
Yash Tripathi <yashtripathi2205@gmail.com>
2023-03-21 13:18:20 +0530

    Second Commit

--- 3_Lists.c
+++ 3_Lists.c
@@ -18,3 +18,27 @@ print(c[0:4])
 print(c[:6])
 print(c[0:])
 print(c[-5:])
+
+# List Methods 
+
+list = [1, 5, 3, 7, 11, 9]
+print(list)
+list_sorted = list.sort() 
+print(list_sorted)
+print(list)
+# list.sort() sorts the original list, no need to store
+
+list.reverse()
+print(list)
+
+list.append(67) # will put '67' at the end of the list
+print(list)
+
+list.insert(2,5) # will insert '5' at the second position
+print(list)
+
+list.pop(3) # will remove element from position third
+print(list)
+
+list.remove(1) # will remove element
+print(list)
