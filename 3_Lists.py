e84aa469b6358cad6c83b5e31ef578e6bac5de61
Yash Tripathi <yashtripathi2205@gmail.com>
2023-03-22 00:32:41 +0530

    Third Commit

--- 3_Lists.py
+++ 3_Lists.py
@@ -4,11 +4,15 @@ a = [1, 2, 3, 40, 5]
 print(a)
 print(a[2])
 print(a[3])
-a[2] = 89
+a[2] = 89      # changes value because lists are mutable
 print(a)
+print("\n------------------------\n")
+
+# A list can consist of any kind of data types
 
 b = [2.56, "India", 45]
 print(b)
+print("\n------------------------\n")
 
 # List slicing 
 
@@ -18,15 +22,17 @@ print(c[0:4])
 print(c[:6])
 print(c[0:])
 print(c[-5:])
+print("\n------------------------\n")
 
 # List Methods 
 
 list = [1, 5, 3, 7, 11, 9]
 print(list)
-list_sorted = list.sort() 
+list_sorted = list.sort()      # list.sort() sorts the original list, no need to store
 print(list_sorted)
 print(list)
 # list.sort() sorts the original list, no need to store
+print("\n------------------------\n")
 
 list.reverse()
 print(list)
