5d069f8e6ddf2bb687b238ede5ad9cda4a65d25d
Yash Tripathi <yashtripathi2205@gmail.com>
2023-03-22 00:26:38 +0530

    Third Commit

--- 2_String_Operations.py
+++ 2_String_Operations.py
@@ -3,22 +3,27 @@
 greeting = "Good evening, "
 name = "Yash"
 print(greeting + name)
+print("\n--------------------\n")
+
 
 
 # Below is a code to show indexing and slicing in strings
 
 a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-print(a[5]) #indexing 
-print(a[0:9]) #slicing
-print(a[0:])
-print(a[:8])
-print(a[-1]) #negative indexing
+print(a[5])           #indexing, would print thr 6th element
+print(a[0:9])         #slicing, would print 0 to 8, first 9 elements
+print(a[0:])          #would print 0 to end i.e. all elements
+print(a[:8])          #would print 0 to 7, first 8 elements
+print(a[-1])          #negative indexing, elements are marked from (-1) to (-n) from right to left
+print("\n--------------------\n")
+
 
 
 # Slicing with skip value
 
-print(a[0:4:2]) # print 0 to 3 BUT print every second character
-print(a[0:20:4]) # print 0 to 19 BUT print every fourth character
+print(a[0:4:2])       # print 0 to 3 BUT print every second character
+print(a[0:20:4])      # print 0 to 19 BUT print every fourth character
+print("\n--------------------\n")
 
 
 # String length 
@@ -26,25 +31,31 @@ print(a[0:20:4]) # print 0 to 19 BUT print every fourth character
 b = "The city of Istanbul was earlier called Constantinople"
 c = "Yash"
 d = "hello"
-print(len(b))
+print(len(b)) 
 print(len(c))
+print("\n--------------------\n")
 
 
 # Other string operations
 
 print(b.endswith("Constantinople"))
-print(c.endswith("h")) #checks whether the string ends with 'h' or not
+print(c.endswith("h"))                     #checks whether the string ends with 'h' or not
 print(c.endswith("a"))
+print("---------")
 
-print(b.count("a")) #counts total number of 'a'
-print(b.count("was")) #counts total number of 'was'
+print(b.count("a"))                        #counts total number of 'a'
+print(b.count("was"))                      #counts total number of 'was'
+print("---------")
 
-print(d.capitalize()) #capitalizes first letter of the string
+print(d.capitalize())                      #capitalizes first letter of the string
+print("---------")
 
-print(b.find("was")) #finds posiion of 'was'
-print(b.find("Yash")) #output is "-1" because not present in string b
+print(b.find("was"))                       #finds posiion of 'was'
+print(b.find("Yash"))                      #output is "-1" because not present in string b
+print("---------")
 
-print(b.replace("Constantinople","what?")) #replaces all occurances the old word with new word
+print(b.replace("Constantinople","what?")) #replaces all occurances of the old word with new word
+print("\n--------------------\n")
 
 
 # escaape sequence 
@@ -55,32 +66,10 @@ print(e)
 #\t for tab (space)
 #\' for single quote
 #\\ for backslash
+print("\n--------------------\n")
 
 
+st = "This  is a string with double spaces"
 
-
-# Replacing a letter with user input
-
-letter = '''Dear <|NAME|>,
-Greetings from ABC coding house. I am happy to tell you about your selection
-You are selected!
-Have a great day ahead!
-Thanks and regards,
-Bill
-Date: <|DATE|>
-'''
-name = input("Enter Your Name\n")
-date = input("Enter Date\n")
-letter = letter.replace("<|NAME|>", name)
-letter = letter.replace("<|DATE|>", date)
-print(letter)
-
-
-
-
-st = "This is a string with double  spaces"
-
-doubleSpaces = st.find("  ")
+doubleSpaces = st.find("  ") # prints the location of double spaces
 print(doubleSpaces)
-
-
