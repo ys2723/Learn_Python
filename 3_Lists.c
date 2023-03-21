60ac01f577db943e1eaa90883879f6b43462da20
Yash Tripathi <yashtripathi2205@gmail.com>
2023-03-21 13:16:58 +0530

    initial commit

---
+++ 3_Lists.c
@@ -0,0 +1,20 @@
+# List
+
+a = [1, 2, 3, 40, 5]
+print(a)
+print(a[2])
+print(a[3])
+a[2] = 89
+print(a)
+
+b = [2.56, "India", 45]
+print(b)
+
+# List slicing 
+
+c = ["New Delhi", "Tokyo", "Beijing", "Berlin", "London", "Moscow"]
+print(c)
+print(c[0:4])
+print(c[:6])
+print(c[0:])
+print(c[-5:])
