d93d16604167791e0ce615985f1db77f09c1c34f
Yash Tripathi <yashtripathi2205@gmail.com>
2023-03-24 19:44:53 +0530

    Second Commit

--- 5-->Tuple.py
+++ 5-->Tuple.py
@@ -4,13 +4,13 @@ a = (9, 1, 4, 7, 8, 44)
 print(a)
 
 b = ()
-print(b) # empy tuple
+print(b)         # empy tuple
 
 c = (1)
-print(c) # not a tuple with single element, not a tuple, its a number
+print(c)         # not a tuple with single element, not a tuple, its a number
 
 d = (1,)
-print(d) # this is a tuple with single element
+print(d)         # this is a tuple with single element
 # tuple with only one element needs a comma
 
 # Tuple methods
@@ -18,4 +18,4 @@ print(d) # this is a tuple with single element
 print(a.count(4)) # will return the number of times '4' occured
 print(a.index(4)) # will return the index of first occurance of '4'
 
-print(sum(a)) # sums all elements in tuple a
+print(sum(a))     # sums all elements in tuple a
