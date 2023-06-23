45a0882092c05b404b6433c7aa2075d4e757e035
Yash Tripathi <yashtripathi2205@gmail.com>
2023-06-23 20:50:28 +0530

    Third Commit

--- 1-->Basic-Codes.py
+++ 1-->Basic-Codes.py
@@ -1,8 +1,8 @@
 # Below is a code to print two numbers while taking input from the user
 
-a = input("Enter first number: ") #input always taken as string
+a = input("Enter first number: ")                               #input always taken as string
 b = input("Enter second number: ")
-a = float(a) #converting string to float
+a = float(a)                                                    #converting string to float
 b = float(b) 
 print('The Sum of the two numbers is ',a+b) 
 print("\n--------------------------------------------------\n")
@@ -10,9 +10,9 @@ print("\n--------------------------------------------------\n")
 
 # Below is a code to find remainder
 
-a = input("Enter first number: ") #input always taken as string
+a = input("Enter first number: ")                                #input always taken as string
 b = input("Enter second number: ")
-a = float(a) #converting string to float
+a = float(a)                                                     #converting string to float
 b = float(b) 
 print("The reaminder is ",a%b)
 print("\n--------------------------------------------------\n")
