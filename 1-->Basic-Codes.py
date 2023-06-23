530e1f1763fa17f104da752600b648d8a379b499
Yash Tripathi <yashtripathi2205@gmail.com>
2023-06-23 20:52:55 +0530

    Fifth Commit

--- 1-->Basic-Codes.py
+++ 1-->Basic-Codes.py
@@ -7,6 +7,7 @@ b = float(b)
 print('The Sum of the two numbers is ',a+b) 
 print("\n--------------------------------------------------\n")
 
+# = Statement (Statements are started with '#', statements are ginoed by the compiler. It is for the reader to understand the codes etc.
 
 # Below is a code to find remainder
 
